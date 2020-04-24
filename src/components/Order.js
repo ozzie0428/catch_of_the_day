4e40cd03d500f66059f8d69e3775757ed7877d76
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2020-04-24 14:40:48 -0400

    set up order, header, inventory components

---
+++ src/components/Order.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Order extends Component {
+    render() {
+        return (
+            <div className="order">
+                <h1>ORDER!!!!</h1>
+            </div>
+        )
+    }
+}
