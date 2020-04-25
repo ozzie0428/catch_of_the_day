9bcc93d9e0a07b9a9f9fec87d332c132a2923f6d
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2020-04-25 19:11:34 -0400

    add fish form

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import AddFishForm from './AddFishForm'
 
 export default class Inventory extends Component {
     render() {
         return (
             <div className="inventory">
-                <h1>Inventory!!!!!!!</h1>
+                <h2>Inventory</h2>
+                <AddFishForm addFish={this.props.addFish} />
+                <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
             </div>
         )
     }
