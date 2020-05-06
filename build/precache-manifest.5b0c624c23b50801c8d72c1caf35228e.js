9fe77ec25a7612ea5e6f8e50b1bfc024aa3ef1a4
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2020-05-06 11:04:09 -0400

    create build

---
+++ build/precache-manifest.5b0c624c23b50801c8d72c1caf35228e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0753f65d22c82906c1d118d899d2d90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f429862a6926446e51d5",
+    "url": "/static/css/main.4ef3e498.chunk.css"
+  },
+  {
+    "revision": "21e3218e2c97fa1332b5",
+    "url": "/static/js/2.1f38088f.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/static/js/2.1f38088f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f429862a6926446e51d5",
+    "url": "/static/js/main.ee30b440.chunk.js"
+  },
+  {
+    "revision": "c0cdb22591de79c18927",
+    "url": "/static/js/runtime-main.7857fc8c.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
